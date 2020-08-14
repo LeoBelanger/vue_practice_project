fffb174bfbc8e33fb805257a87c07006e5018862
Leo Belanger <leobelanger1996@gmail.com>
2020-08-14 12:38:27 -0700

    added a todo item component, array of todos are binded to the Todos attribute, then they are looped through within Todos.vue. For each todo, a TodoItem is added to the dom by binding the todo to the todoItem attribute

---
+++ src/components/Todos.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <!-- Loop through todos passed in as props. Remember to add key -->
+    <div v-bind:key="todo.id" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: {
+    TodoItem,
+  },
+  props: ["todos"], //Receive props from parent (App.vue) as array
+};
+</script>
+
+<style scoped>
+</style>
